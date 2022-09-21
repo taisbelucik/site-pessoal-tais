283e72318367a50719e7e4e6678d3035ed4d03d0
taisbelucik <taisbelucik@hotmail.com>
2022-09-20 23:05:42 -0300

    scroll suave com javaScript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const menuLinks = document.querySelectorAll('.menu a');
 
+menuLinks.forEach((link) => {  //para cada link dessa lista, verificar se foi clicado ou não
+    link.addEventListener("click",scrollToSection);
+});
+
+function scrollToSection(event){
+    event.preventDefault()
+    const element = event.target
+    const id = element.getAttribute('href')
+    const to = document.querySelector(id).offsetTop
+    
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    })
+}
