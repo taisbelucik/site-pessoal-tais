bf8fd9315385d1d3840c9e372b4a0bd2a30b395d
taisbelucik <taisbelucik@hotmail.com>
2022-09-22 10:13:19 -0300

    ajustes no layout responsivo

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const menuLinks = document.querySelectorAll('.menu a');
 
 menuLinks.forEach((link) => {  //para cada link dessa lista, verificar se foi clicado ou não
-    link.addEventListener("click",scrollToSection);
+    link.addEventListener("click",scrollToSection); // adicionar um evento de click e executar a função scrollToSection
 });
 
 function scrollToSection(event){
-    event.preventDefault()
-    const element = event.target
-    const id = element.getAttribute('href')
-    const to = document.querySelector(id).offsetTop
+    event.preventDefault() // previne o padrão
+    const element = event.target // busca o alvo que quero chegar com o clique
+    const id = element.getAttribute('href') // pega o valor do href dos elementos das sessões
+    const to = document.querySelector(id).offsetTop // descobre a altura do topo até o link clicado
     
+    //informa para onde deve ir o scroll
     window.scroll({
-        top: to,
-        behavior: "smooth"
+        top: to, // faz o scroll do topo da página até o id do link clicado
+        behavior: "smooth" // para fazer o scroll de forma suave
     })
 }
+
+
+
+
